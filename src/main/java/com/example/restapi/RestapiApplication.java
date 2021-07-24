15fdc845e14a91a8434e63d143ce8fc660193523
Siramol Chaijindawat <siramol.cha@ngernturbo.com>
2021-07-24 15:49:35 +0700

    bean random

--- src/main/java/com/example/restapi/RestapiApplication.java
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -3,9 +3,17 @@ package com.example.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class RestapiApplication {
+
+	@Bean
+	public Random createRandom() {
+		return new Random();
+	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx
@@ -14,7 +22,7 @@ public class RestapiApplication {
 //		GenerateIdService service = ctx.getBean(GenerateIdService.class);
 //		System.out.println("====>" + service.counter);
 //		service.counter ++;
-
+//
 //		GenerateIdService service2 = ctx.getBean(GenerateIdService.class);
 //		System.out.println("====>" + service2.counter);
 	}
--- src/test/java/com/example/restapi/number/IdControllerTest.java
+++ src/test/java/com/example/restapi/number/IdControllerTest.java
@@ -4,9 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class IdControllerTest {
@@ -14,8 +20,13 @@ public class IdControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @MockBean
+    private Random stubRandom;
+
     @Test
     void generateId() {
+        // Arrange
+        when(stubRandom.nextInt(anyInt())).thenReturn(7);
         // Act
         DataResponse result
                 = restTemplate.getForObject("/id", DataResponse.class);
